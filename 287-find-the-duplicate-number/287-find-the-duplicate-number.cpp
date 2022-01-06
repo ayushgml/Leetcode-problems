8c28760337010dda03b60a333c0dd80c242b76f7
Ayush Gupta <72748253+ayushgml@users.noreply.github.com>
2022-01-06 14:21:38 +0530

    Time: 376 ms (5.02%), Space: 83.9 MB (14.48%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,8 +4,6 @@ public:
         unordered_map<int,int> ump;
         for(int i=0;i<nums.size();i++){
             ump[nums[i]]++;
-        }
-        for(int i=0;nums.size();i++){
             if(ump[nums[i]]>1)
                 return nums[i];
         }
