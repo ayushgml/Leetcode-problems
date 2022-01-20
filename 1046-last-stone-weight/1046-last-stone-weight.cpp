aee3a5dd3c43374eae9391d0eb89299541079fe4
Ayush Gupta <72748253+ayushgml@users.noreply.github.com>
2022-01-20 17:07:23 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (77.51%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -3,9 +3,8 @@ public:
     int lastStoneWeight(vector<int>& stones) {
         sort(stones.begin(),stones.end());
         while(stones.size()>1){
-            int x = stones[stones.size()-1] - stones[stones.size()-2];
-            stones.erase(stones.end()-2,stones.end());
-            stones.push_back(x);
+            stones.push_back(stones[stones.size()-1] - stones[stones.size()-2]);
+            stones.erase(stones.end()-3,stones.end()-1);
             sort(stones.begin(),stones.end());
         }
         return (stones[0]);
