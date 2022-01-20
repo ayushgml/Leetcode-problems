3cf12d2d0de9a13a00b481fb3efa5894ce6db24c
Ayush Gupta <72748253+ayushgml@users.noreply.github.com>
2022-01-20 16:45:06 +0530

    Time: 24 ms (91.50%), Space: 90.1 MB (14.49%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> m;
+        for(int i=0;i<nums1.size();i++){
+            m.push_back(nums1[i]);
+        }
+        for(int i=0;i<nums2.size();i++){
+            m.push_back(nums2[i]);
+        }
+        sort(m.begin(),m.end());
+        if(m.size()%2!=0)
+            return double(m[((m.size())/2)]);
+        else
+            return double((m[((m.size())/2)] + m[((m.size())/2)-1]))/2;    
+    }
+};
