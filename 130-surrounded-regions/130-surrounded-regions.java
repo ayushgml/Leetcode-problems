41962d9b75146420421bc0db5a2f9e4260c6dc7d
Ayush Gupta <72748253+ayushgml@users.noreply.github.com>
2022-07-04 20:09:36 +0530

    Time: 3 ms (54.67%), Space: 51.5 MB (57.62%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void solve(char[][] board) {
+        if(board.length==0 || board[0].length==0) return;
+        
+        int rows = board.length;
+        int columns = board[0].length;
+        
+        for(int i=0;i<rows;i++){
+            if(board[i][0]=='O') boundaryDFS(board, i, 0);
+            if(board[i][columns-1]=='O') boundaryDFS(board, i, columns-1);
+        }
+        
+        for(int j=0;j<columns;j++){
+            if(board[0][j]=='O') boundaryDFS(board, 0, j);
+            if(board[rows-1][j]=='O') boundaryDFS(board, rows-1, j);
+        }
+        
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='#') board[i][j]='O';
+            }
+        }
+    }
+    
+    public void boundaryDFS(char[][] board, int i, int j){
+        if(board[i][j]=='O') board[i][j]='#';
+        if(i>0 && board[i-1][j]=='O') boundaryDFS(board,i-1,j);
+        if(i<board.length-1 && board[i+1][j]=='O') boundaryDFS(board,i+1,j);
+        if(j>0 && board[i][j-1]=='O') boundaryDFS(board,i,j-1);
+        if(j<board[i].length-1 && board[i][j+1]=='O') boundaryDFS(board,i,j+1);
+    }
+}
